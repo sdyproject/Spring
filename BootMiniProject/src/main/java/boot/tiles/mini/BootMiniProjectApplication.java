655458c1146601be290f2e6d52492f91ff7b7e68
sdyproject <126428651+sdyproject@users.noreply.github.com>
2023-04-18 23:45:23 +0900

    miniproject(1)

---
+++ BootMiniProject/src/main/java/boot/tiles/mini/BootMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package boot.tiles.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMiniProjectApplication.class, args);
+	}
+
+}
